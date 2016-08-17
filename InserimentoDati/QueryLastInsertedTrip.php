056648e1b199df6c57f2152db7f01f1994c24e3e
Giacomo <g.lanciano1@gmail.com>
2016-08-17 14:06:34 +0200

    add query to get user last inserted trip

---
+++ InserimentoDati/QueryLastInsertedTrip.php
@@ -0,0 +1,55 @@
+<?php
+
+//mysqli_select_db();
+
+
+define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
+require_once(__ROOT__.'/takeatrip_db_config.php');
+
+$conn = mysql_connect(HOSTNAME,DBUSER,DBPASS,DBNAME);
+
+if($conn == null){
+	echo "connessione null";
+}
+
+
+$email = $_POST["email"];
+
+
+$q = mysql_query("
+SELECT 
+    *
+FROM
+    takeatrip_db.Viaggio v,
+    takeatrip_db.PartePer p
+WHERE
+    v.codice = p.codiceViaggio
+    AND p.emailProfilo = '$email'
+ORDER BY v.timestamp DESC
+LIMIT 1");
+
+
+
+				
+				
+if($q==null){
+	echo("variabile nulla");
+}
+
+while($e=mysql_fetch_assoc($q)){
+        $output[]=$e;
+}
+
+
+if($output != null){
+	print(json_encode($output));
+}
+else{
+	print(json_encode($output));
+}
+
+
+
+
+mysql_close($conn);
+?>
