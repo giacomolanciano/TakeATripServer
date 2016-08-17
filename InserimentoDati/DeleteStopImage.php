110563760fb2b7cb270667e525a24e335c9e7a3e
Giacomo <g.lanciano1@gmail.com>
2016-08-17 17:55:39 +0200

    add query to delete stops contents

---
+++ InserimentoDati/DeleteStopImage.php
@@ -0,0 +1,47 @@
+<?php
+
+//mysqli_select_db();
+
+
+define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
+require_once(__ROOT__.'/takeatrip_db_config.php');
+
+$conn = mysql_connect(HOSTNAME,DBUSER,DBPASS,DBNAME);
+
+if($conn == null){
+	echo "connessione null";
+}
+
+
+$codiceViaggio = $_POST["codiceViaggio"];
+$url = $_POST["url"];
+
+
+$q = mysql_query("
+DELETE FROM takeatrip_db.ImmagineViaggio 
+WHERE
+    codiceViaggio = '$codiceViaggio'
+    AND urlImmagineViaggio = '$url';");
+
+
+if($q==null){
+	echo("variabile nulla");
+}
+
+while($e=mysql_fetch_assoc($q)){
+        $output[]=$e;
+}
+
+
+if($output != null){
+	print(json_encode($output));
+}
+else{
+	print(json_encode($output));
+}
+
+
+
+
+mysql_close($conn);
+?>
---
+++ InserimentoDati/DeleteStopNote.php
@@ -0,0 +1,49 @@
+<?php
+
+//mysqli_select_db();
+
+
+define('__ROOT__', dirname(dirname(dirname(__FILE__)))); 
+require_once(__ROOT__.'/takeatrip_db_config.php');
+
+$conn = mysql_connect(HOSTNAME,DBUSER,DBPASS,DBNAME);
+
+if($conn == null){
+	echo "connessione null";
+}
+
+
+$codiceViaggio = $_POST["codiceViaggio"];
+$ordineTappa = $_POST["ordineTappa"];
+$nota = $_POST["nota"];
+
+
+$q = mysql_query("
+DELETE FROM takeatrip_db.NotaTappa 
+WHERE
+    codViaggio = '$codiceViaggio'
+    AND ordineTappa = '$ordineTappa'
+    AND nota = '$nota';");
+
+
+if($q==null){
+	echo("variabile nulla");
+}
+
+while($e=mysql_fetch_assoc($q)){
+        $output[]=$e;
+}
+
+
+if($output != null){
+	print(json_encode($output));
+}
+else{
+	print(json_encode($output));
+}
+
+
+
+
+mysql_close($conn);
+?>
