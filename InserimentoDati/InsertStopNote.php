91a9e805e573ec93a271ee8385f5620258d741cc
Giacomo <g.lanciano1@gmail.com>
2016-08-20 12:38:14 +0200

    fix InsertStopNode.php

--- InserimentoDati/InsertStopNote.php
+++ InserimentoDati/InsertStopNote.php
@@ -7,9 +7,9 @@ require_once(__ROOT__.'/takeatrip_db_config.php');
 
 $conn = mysql_connect(HOSTNAME,DBUSER,DBPASS,DBNAME);
 
-if($conn == null){
-	echo "connessione null";
-}
+if($conn == null){ 
+	echo "connessione null"; 
+} 
 
 
 $ordine = $_POST["ordine"];
@@ -17,11 +17,10 @@ $codiceViaggio = $_POST["codiceViaggio"];
 $emailProfilo = $_POST["emailProfilo"];
 $timestamp = $_POST["timestamp"];
 $nota = $_POST["nota"];
-$livelloCondivisione = $_POST["livelloCondivisione"]
-
+$livelloCondivisione = $_POST["livelloCondivisione"];
 
 $sql = "INSERT INTO takeatrip_db.NotaTappa VALUES('$emailProfilo','$codiceViaggio','$ordine','$timestamp','$nota','$livelloCondivisione');"; 
-$mysql_result=mysql_query($sql,$conn) or die(mysql_error()); 
+$mysql_result=mysql_query($sql,$conn) or die(mysql_error());
 
 echo "OK"; 
 
